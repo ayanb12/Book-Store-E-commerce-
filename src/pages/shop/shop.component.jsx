a3d89fb02e06f277c70ad10f819421f30c1a1269
Ayan Banerjee <ayanbbb354@gmail.com>
2020-02-25 23:58:32 +0530

    Added some features

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,26 +1,10 @@
-import React, { Component } from "react";
-import ShopData from "./shop.data";
-import CollectionPreview from "./../../components/collection-preview/collection-preview.component";
+import React from "react";
+import CollectionsOverview from "./../../components/collections-overview/collections-overview.component";
 
-class ShopPage extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collections: ShopData
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const ShopPage = () => (
+  <div className="shop-page">
+    <CollectionsOverview />
+  </div>
+);
 
 export default ShopPage;
